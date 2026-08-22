0b99b47dc163bf1774e04c1fcfab18545d956163
kushagras <kushagras652@gmail.com>
2026-08-22 13:15:39 +0530

    target manager dependencies setup

---
+++ target_manager.py
@@ -0,0 +1,3 @@
+from typing import List,Dict,Tuple
+from target import TargetConfig,run_target,BASE_SYSTEM_PROMPT
+from patches import compose_input_guards,compose_output_guards,INPUT_GUARDS,OUTPUT_GUARDS
